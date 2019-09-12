3695f01a455a849f116613ab64e5202ecf15c55b
Rui Ramos <rui@ometria.com>
2019-09-12 15:14:08 +0100

    Add document and app

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+class MyApp extends App {
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return <Component {...pageProps} />;
+  }
+}
+
+export default MyApp;
