8f04e188704d0f65ae74ef3b42ec3bff19bb8c08
Rui Ramos <rui@ometria.com>
2019-09-12 21:43:07 +0100

    Removes flush

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/styles';
-import flush from 'styled-jsx/server';
 
 class MyDocument extends Document {
   render() {
@@ -69,7 +68,6 @@ MyDocument.getInitialProps = async ctx => {
     styles: (
       <React.Fragment>
         {sheets.getStyleElement()}
-        {flush() || null}
         {initialProps.styles}
       </React.Fragment>
     ),
